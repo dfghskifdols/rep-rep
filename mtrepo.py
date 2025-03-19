ea892be09a83745951890292b788bb97a5537073
dfghskifdols <dfsdsgfdgfsg@gmail.com>
2025-03-19 21:43:41 +0200

    Update mtrepo.py

--- mtrepo.py
+++ mtrepo.py
@@ -19,15 +19,10 @@ async def handle_report(message: types.Message):
         if message.reply_to_message:
             reported_message = message.reply_to_message
             message_link = f"https://t.me/{message.chat.username}/{reported_message.message_id}"  # Формируем ссылку на сообщение
-            # Экранируем точку в ссылке
-            message_link = message_link.replace('.', r'\.')  
-            report_text += f"\n\nСсылка на сообщение: {message_link}"
+            report_text += f"\n\nСсылка на сообщение: <a href='{message_link}'>Перейти к сообщению</a>"
 
-        # Экранируем все специальные символы для MarkdownV2
-        report_text = report_text.replace('*', r'\*').replace('_', r'\_').replace('[', r'\[').replace(']', r'\]')
-
-        # Отправляем репорт в группу администрации
-        await bot.send_message(ADMIN_CHAT_ID, report_text, parse_mode=ParseMode.MARKDOWN_V2)
+        # Отправляем репорт в группу администрации с использованием HTML-форматирования
+        await bot.send_message(ADMIN_CHAT_ID, report_text, parse_mode=ParseMode.HTML)
 
         # Подтверждаем пользователю, что репорт отправлен
         await message.reply("Репорт успешно отправлен!")
