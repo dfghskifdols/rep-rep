b98d8d887db7f6c524f5633bbec1c35ff06479aa
dfghskifdols <dfsdsgfdgfsg@gmail.com>
2025-03-23 17:58:46 +0200

    Update mtrepo.py

--- mtrepo.py
+++ mtrepo.py
@@ -71,6 +71,7 @@ async def handle_report(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Проверка, что запрос пришел от пользователя, который отправил репорт
     if query.from_user.id != user_id:
         # Показываем всплывающее сообщение, если это не тот, кто отправил репорт
+        logger.info("Попытка взаимодействия с чужим репортом!")
         await query.answer(text="❌ Нельзя жмякать чужие репорты!", show_alert=True)
         return
 
