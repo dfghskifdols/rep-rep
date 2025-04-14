0fda0673e21e91fb4a83cc8df47c2a1c1a14fa9e
dfghskifdols <dfsdsgfdgfsg@gmail.com>
2025-04-14 17:16:45 +0300

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 
 api_id = 20375319  # Твій api_id, отриманий на my.telegram.org
 api_hash = '4cb9df2254ee99282e0bd8b00ed1a636'  # Твій api_hash
-phone_number = '+380 68 678 24 26'  # Твій номер телефону
+phone_number = '+380686782426'  # Твій номер телефону
 
 # Створюємо об'єкт клієнта
 app = Client("userbot", api_id=api_id, api_hash=api_hash)
