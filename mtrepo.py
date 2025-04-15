63738932df423c3cc850936bd340bd5bb97a6b36
dfghskifdols <dfsdsgfdgfsg@gmail.com>
2025-04-15 17:39:06 +0300

    Update mtrepo.py

--- mtrepo.py
+++ mtrepo.py
@@ -588,9 +588,9 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             count = next((row["count"] for row in all_rows if row["accepted_by"] == current_user_id), 0)
 
             if position:
-                leaderboard += f"\nТвое место: {position} - {count}❇️"
+                leaderboard += f"\nТвое место: {position} - {count}❇"
             else:
-                leaderboard += f"\nТвое место: {len(all_rows) + 1} - 0❇️"
+                leaderboard += f"\nТвое место: {len(all_rows) + 1} - 0❇"
 
         await update.message.reply_text(leaderboard, parse_mode=ParseMode.HTML)
         return
