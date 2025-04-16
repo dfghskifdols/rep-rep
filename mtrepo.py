086428f76c5a5accf43dd58f8647f19f4460bad0
dfghskifdols <dfsdsgfdgfsg@gmail.com>
2025-04-16 19:41:39 +0300

    Update mtrepo.py

--- mtrepo.py
+++ mtrepo.py
@@ -681,7 +681,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
             if rows:
                 active_promos = "\n".join([
-                    f"{'🔅' if row['created_by_bot'] else '🔆'}<code>(row['code'])}</code>" for row in rows
+                    f"{'🔅' if row['created_by_bot'] else '🔆'}<code>{row['code']}</code>" for row in rows
                 ])
             else:
                 active_promos = "🔸Нет активных промокодов."
