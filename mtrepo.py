b5ff3bcddb90579780cbb0e551552de75eddcdcc
dfghskifdols <dfsdsgfdgfsg@gmail.com>
2025-05-17 10:46:56 +0300

    Update mtrepo.py

--- mtrepo.py
+++ mtrepo.py
@@ -54,7 +54,8 @@ LEVEL_REQUIREMENTS = {
     27: {"coins": 3500, "tickets": 3, "drops": 1},
     28: {"coins": 4000, "tickets": 3, "drops": 2},
     29: {"coins": 4500, "tickets": 3, "drops": 2},
-    30: {"coins": 5000, "tickets": 3, "drops": 2}
+    30: {"coins": 5000, "tickets": 3, "drops": 2},
+    31: {"coins": 5500, "tickets": 4, "drops": 3}
 }
 
 pending_reports = set()
